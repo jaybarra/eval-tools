ad8f4b37518d4c4798819935504dfb4c7aa9e44d
Jay Barra <jay.barra@nasa.gov>
2020-08-26 21:37:51 -0400

    random recursive work

--- src/maths/core.clj
+++ src/maths/core.clj
@@ -1,2 +1,46 @@
-(ns maths.core
-  (:require [maths.pso :as pso]))
+(ns maths.core)
+
+;; Factorial
+(defn factorial
+  [n]
+  (let [big-n (bigint n)]
+    (if (pos? big-n)
+      (* big-n (factorial (dec big-n)))
+      1)))
+
+;; Fibonacci
+(def fib
+  (memoize
+    (fn [n]
+      (if (< n 2)
+        1
+        (+ (fib (dec (dec n))) (fib (dec n)))))))
+
+;; Collatz Conjecture
+(def collatz
+  (memoize
+    (fn [n]
+      (let [big-n (bigint n)]
+        (if (= 1 big-n)
+          1
+          (collatz
+            (if (even? big-n)
+              (/ big-n 2)
+              (/ (inc (* 3 big-n)) 2))))))))
+
+(defn tower
+  "Tower of Hanoi solver. Returns list of movements."
+  [n source dest aux]
+  (cond
+    (= n 1) [[source dest]]
+    (> n 1) (concat (tower (dec n) source aux dest)
+                    (tower 1 source dest aux)
+                    (tower (dec n) aux dest source))
+    :else (throw "Values smaller than 1 are not allowed.")))
+
+(defn print-tower!
+  "Takes list of tower moves and prints the solving sequence."
+  [tower-moves]
+  (println "== Tower of Hanoi Solver ==")
+  (doseq [[s d] tower-moves] (printf "Move disk from %s to %s\n" s d)))
+
--- test/maths/core_test.clj
+++ test/maths/core_test.clj
@@ -1,6 +1,22 @@
 (ns maths.core-test
   (:require [clojure.test :refer :all]
-            [maths.core :refer :all]))
+            [maths.core :as core]))
 
+(deftest fibo-test
+  (is (not= nil core/fib 1))
+  (is (pos? (core/fib 1))))
 
+(deftest collatz-test
+  (is (= 1 (core/collatz 1)))
+  (is (= 1 (core/collatz 2)))
+  (is (= 1 (core/collatz 3)))
+  (is (= 1 (core/collatz 23541561999213451235415619992135234124123))))
+
+(deftest tower-test
+  (is (= [["S" "D"]]
+         (core/tower 1 "S" "D" "A")))
+  (is (= [["S" "A"]
+          ["S" "D"]
+          ["A" "D"]]
+         (core/tower 2 "S" "D" "A"))))
 
