edd3401c0c56ff7f4cd1ceb8c291e7491ac45255
Jay Barra <jay.f.barra@gmail.com>
2021-06-15 14:38:13 -0400

    set default log level

--- dev/user.clj
+++ dev/user.clj
@@ -6,9 +6,17 @@
    [clojure.stacktrace :as st]
    [clojure.tools.namespace.repl :refer [refresh]]
    [integrant.repl :as ig-repl]
-   [eval.system :refer [config]]))
+   [eval.system :refer [config]]
+   [taoensso.timbre :as log]))
 
 (ig-repl/set-prep! config)
+
+(defn set-logging-level!
+  "Set the log level for the system"
+  [level]
+  (log/set-level! level))
+
+(set-logging-level! (get-in (config) [:app/logging :level] :debug))
 
 (defn go
   []
