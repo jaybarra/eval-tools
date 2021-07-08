2d6df14268b9855bb2c5f050b7f127f055cc42ef
Jay Barra <jay.f.barra@gmail.com>
2021-07-07 22:27:07 -0400

    continuing code coverage gains

--- test/unit/eval/db/document_store_test.clj
+++ test/unit/eval/db/document_store_test.clj
@@ -4,4 +4,16 @@
    [eval.db.document-store :as ds]))
 
 (deftest create-document-store-test
-  (is (not (nil? (ds/create-document-store {:type :noop})))))
+  (let [dbstore (ds/create-document-store {:type :noop})]
+    (is (not (nil? dbstore)))
+
+    (.save! dbstore {})
+    (.query dbstore {})
+    (.halt! dbstore)))
+
+(deftest stop-document-store-test
+  (let [dbstore (reify ds/DocumentStore
+                  (save! [this document] nil)
+                  (query [this query] nil)
+                  (halt! [this] (is (true? true) "no exception thrown")))]
+    (ds/stop-document-store dbstore)))
