b1a6ae6cf60b08f80c5d38ced92def2cff9f78e8
Jay Barra <jay.f.barra@gmail.com>
2022-01-10 09:20:20 -0500

    fixed tests and builds

---
+++ components/test-helpers/src/eval/test_helpers/core.clj
@@ -0,0 +1,16 @@
+(ns eval.test-helpers.core
+  (:require
+   [clojure.math.numeric-tower :as math]))
+
+(defn within?
+  "Returns true if two numbers are nearly equal within a given delta.
+  Should only be used for 6 significant digits or fewer.
+
+  Examples:
+  ```clojure
+  (is (true?  (within? 0.1 2.4 2.41)))
+  (is (false? (within? 2 10 3)))
+  ```"
+  [^Double delta a b]
+  (<= (- (double (math/abs (- (double a) (double b)))) 1e-7)
+      (double delta)))
--- projects/cmr-client/build.clj
+++ projects/cmr-client/build.clj
@@ -1,39 +1,22 @@
 (ns build
+  "CMR Client
+  
+  clojure -T:build ci"
   (:require 
    [clojure.tools.build.api :as b]
    [org.corfield.build :as bb]))
 
 (def lib 'eval/cmr-client)
 (def version (format "1.0.%s" (b/git-count-revs nil)))
-(def class-dir "target/classes")
-(def basis (b/create-basis {:project "deps.edn"}))
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 
-#_(defn clean
-  "Cleans the `target` directory."
-  [_]
-  (println "Cleaning target directory...")
-  (b/delete {:path "target"}))
-
-#_(defn uber
-  "Builds an uberjar for the project."
-  [_]
-  (clean nil)
-  (println "Copying source and resources...")
-  (b/copy-dir {:src-dirs ["src" "resources"]
-               :target-dir class-dir})
-  (println "Compiling...")
-  (b/compile-clj {:basis basis
-                  :src-dirs ["src"]
-                  :class-dir class-dir})
-  (println "Assembling uberjar...")
-  (b/uber {:class-dir class-dir
-           :uber-file uber-file
-           :basis basis
-           :main 'eval.cmr.cli.main})
-  (println "Build complete"))
-(defn uber [opts]
-  (bb/uber {:class-dir class-dir
-            :uber-file uber-file
-            :basis basis
-            :main 'eval.cmr.cli.main}))
+(defn ci "Run the CI pipeline of tests (and build the Uber JAR)." [opts]
+  (let [opts (or opts {})]
+    (-> opts
+        (assoc :lib lib 
+               :version version 
+               :uber-file uber-file
+               :main 'eval.cmr.cli.main)
+        (bb/run-tests)
+        (bb/clean)
+        (bb/uber))))
