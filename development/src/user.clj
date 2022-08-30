9b676bffef083f4328550f22fa6b7461a3d93a65
Jay Barra <jay.barra@nasa.gov>
2022-08-30 11:46:54 -0400

    updated developer settings for repl commands

--- development/src/user.clj
+++ development/src/user.clj
@@ -1,9 +1,21 @@
 (ns user
   (:require
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.tools.namespace.repl :refer [refresh]]))
 
 (defn banner
   "Print the banner."
   []
   (when-let [banner (io/resource "banner.txt")]
     (println (slurp banner))))
+
+(defn start
+  []
+  (refresh))
+
+(defn stop
+  [])
+
+(defn reset
+  []
+  (refresh))
